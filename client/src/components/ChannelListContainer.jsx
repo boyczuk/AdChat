16c6304d6d26c6fed3b6af7f634e0b8564fe7622
Adlai <adlaibridsonboyczuk@gmail.com>
2022-11-28 16:25:43 -0500

    Title change

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -25,7 +25,7 @@ const SideBar = ({ logout }) => (
 
 const CompanyHeader = () => (
     <div className="channel-list__header">
-        <p className="channel-list__header__text">Boyczuk Chat</p>
+        <p className="channel-list__header__text">AdChat</p>
     </div>
 )
 
